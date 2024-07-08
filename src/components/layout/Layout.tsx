26c659c2713269cc261762436ab6b03e003d3336
dev.arthur <contato.arthurreis222@gmail.com>
2024-07-08 10:19:45 -0300

    feat: add footer e nav links ao layout

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -2,6 +2,8 @@ import { ReactNode } from 'react';
 import Logo from '../../assets/logo.svg';
 import * as S from './styles';
 import { Container } from '../../styles/container';
+import { Link } from 'react-router-dom';
+import { BellIcon } from 'lucide-react';
 
 export interface ILayout {
   children: ReactNode;
@@ -13,10 +15,19 @@ export const Layout = ({ children }: ILayout) => {
       <S.LayoutHeader>
         <S.LayoutContent>
           <img src={Logo} alt="logo planner" />
-          <nav></nav>
+          <S.LayoutNav>
+            <Link to="/dashboard">Início</Link>
+            <Link to="/task">Tarefa</Link>
+            <Link to="/sync">Sincronizar</Link>
+            <Link to="/late">
+              <BellIcon size={24} />
+              <span>2</span>
+            </Link>
+          </S.LayoutNav>
         </S.LayoutContent>
       </S.LayoutHeader>
       <Container>{children}</Container>
+      <S.LayoutFooter>@Planner - Organizando sua vida</S.LayoutFooter>
     </S.LayoutContainer>
   );
 };
--- src/components/layout/styles.tsx
+++ src/components/layout/styles.tsx
@@ -13,10 +13,48 @@ export const LayoutContainer = styled.div`
 export const LayoutHeader = styled.header`
   width: 100%;
   height: 4.5rem;
+  border-bottom: 2px solid var(--violet-800);
 `;
 
 export const LayoutContent = styled(Container)`
   padding-block: 1.5rem;
   align-items: center;
   justify-content: space-between;
+`;
+
+export const LayoutNav = styled.nav`
+  display: flex;
+  gap: 1.125rem;
+
+  a {
+    transition: all 0.3s;
+    &:hover {
+      color: var(--violet-text-200);
+    }
+    &:last-child {
+      position: relative;
+      span {
+        padding: 0.5rem;
+        background-color: var(--violet-700);
+        border-radius: 50%;
+        font-size: 0.625rem;
+        height: 1.5rem;
+        width: 1.5rem;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        position: absolute;
+        top: -12px;
+        right: -12px;
+      }
+    }
+  }
+`;
+
+export const LayoutFooter = styled.footer`
+  height: 2.5rem;
+  text-align: center;
+  p {
+    font-size: 0.75rem;
+  }
 `;
