d11e1717ec540d8f80897a7be8202a3de6748328
dev.arthur <contato.arthurreis222@gmail.com>
2024-07-09 15:09:01 -0300

    feat: buscar tarefa por id e renderizar nos inputs

--- src/pages/edit/Edit.tsx
+++ src/pages/edit/Edit.tsx
@@ -5,12 +5,38 @@ import { Form } from '../../styles/form';
 import { Title } from '../../styles/title';
 import { Input, Select, Textarea } from '../../components';
 import { Button, GhostButton } from '../../styles/button';
-
 import { Checkbox } from './session/checkbox/Checkbox';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { TaskService } from '../../services/task/TaskService';
+import { Task, TypeTask } from '../../types/task';
+import { useParams } from 'react-router-dom';
+import { format } from 'date-fns';
 
 export const Edit = () => {
-  const [done, setDone] = useState<boolean>(false);
+  const { id } = useParams();
+  const [task, setTask] = useState<Task>();
+  const [type, setType] = useState<TypeTask>();
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+  const [when, setWhen] = useState('');
+  const [done, setDone] = useState<boolean>();
+
+  const fetchTask = async () => {
+    await TaskService.getById(id as string).then((response) => {
+      setTask(response);
+      setTitle(response.title);
+      setDescription(response.description);
+      setWhen(format(new Date(response.when), "yyyy-MM-dd'T'HH:mm"));
+      setType(response.type);
+      setDone(response.done);
+    });
+  };
+
+  useEffect(() => {
+    fetchTask();
+  }, []);
+  console.log(task);
+
   return (
     <Content>
       <Form>
@@ -20,16 +46,39 @@ export const Edit = () => {
             <X size={24} />
           </button>
         </Box>
-        <Select label="Categoria">
+        <Select
+          label="Categoria"
+          onChange={(e) => setType(e.target.value as TypeTask)}
+          value={type}
+        >
           <option>Selecione</option>
+          <option value="study">Estudos</option>
+          <option value="gym">Academia</option>
+          <option value="work">Trabalho</option>
+          <option value="personal">Pessoal</option>
+          <option value="travel">Viagem</option>
         </Select>
-        <Input label="Título" placeholder="Título da tarefa" type="text" />
+        <Input
+          label="Título"
+          placeholder="Título da tarefa"
+          type="text"
+          onChange={(e) => setTitle(e.target.value)}
+          value={title}
+        />
+
         <Textarea
           label="Descrição"
           placeholder="Descrição da tarefa"
           rows={6}
+          onChange={(e) => setDescription(e.target.value)}
+          value={description}
         />
-        <Input label="Data" type="datetime-local" />
+        <Input
+          label="Data"
+          type="datetime-local"
+          onChange={(e) => setWhen(e.target.value)}
+          value={when}
+        />
         <Box align="center" justify="space-between">
           <Box>
             <Checkbox
--- src/services/task/TaskService.ts
+++ src/services/task/TaskService.ts
@@ -8,18 +8,24 @@ const getByFilter = async (filter: FilterOptions): Promise<Task[] | []> => {
   return data;
 };
 
+const getById = async (id: string): Promise<Task> => {
+  const { data } = await Api.get(`/task/${id}`);
+  return data;
+};
+
 const late = async (): Promise<Task[] | []> => {
   const { data } = await Api.get('/task/late');
   return data;
 };
 const create = async (
-  params: Pick<Task, 'title' | 'description' | 'when' | "type">
+  params: Pick<Task, 'title' | 'description' | 'when' | 'type'>
 ): Promise<void> => {
   await Api.post('/task', params);
 };
 
 export const TaskService = {
   getByFilter,
+  getById,
   late,
   create,
 };
