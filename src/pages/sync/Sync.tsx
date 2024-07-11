4d8ccd9d3e09189b5276169030df19c3bce066cf
dev.arthur <contato.arthurreis222@gmail.com>
2024-07-11 13:52:24 -0300

    feat: adicionar qr code

--- src/pages/sync/Sync.tsx
+++ src/pages/sync/Sync.tsx
@@ -1,15 +1,19 @@
+import QRCode from 'qrcode.react';
+import { useAuth } from '../../hooks/useAuth';
 import { Box } from '../../styles/box';
 import { Content } from '../../styles/content';
 import { Title } from '../../styles/title';
 
 export const Sync = () => {
+  const { user } = useAuth();
   return (
     <Content>
-      <Box direction="column" gap="24px" justify='center' align='center'>
+      <Box direction="column" gap="24px" justify="center" align="center">
         <Box direction="column" gap="6px">
           <Title>Capture o QRCODE pelo app</Title>
           <p>Suas tarefas serão sincronizadas</p>
         </Box>
+        {user && <QRCode size={320} value={user.email}></QRCode>}
       </Box>
     </Content>
   );
